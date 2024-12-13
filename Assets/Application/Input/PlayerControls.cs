488615abf07ea5458a0da6581d1a979a171df09c
1102-Nivera-Evelynne <enivera1@gmail.com>
2024-12-13 10:08:48 -0800

    Fixed "sliding" while stunned

--- Assets/Application/Input/PlayerControls.cs
+++ Assets/Application/Input/PlayerControls.cs
@@ -14,6 +14,7 @@ public class PlayerControls : MonoBehaviour
     private Vector2 movement;
     public Animator animator;
     public bool isStunned;
+    private RigidbodyConstraints2D originalConstraints;
     
 
     //Start is called before the first frame update
@@ -88,8 +89,14 @@ public class PlayerControls : MonoBehaviour
         float originalSpeed = currentSpeed; 
         currentSpeed = 0;
         animator.SetFloat("BaseSpeed", 0);
+        originalConstraints = rb.constraints;
 
-        yield return new WaitForSeconds(stunDuration); 
+        rb.constraints = RigidbodyConstraints2D.FreezePositionX | RigidbodyConstraints2D.FreezePositionY | RigidbodyConstraints2D.FreezeRotation;
+
+
+        yield return new WaitForSeconds(stunDuration);
+
+        rb.constraints = originalConstraints;
 
         currentSpeed = originalSpeed; 
          isStunned = false;
